c56e8041341a5b59196623dac9e24759106d4697
spcymshrm <joe.jordan1994@gmail.com>
2024-06-11 14:11:55 -0400

    Finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-    
+const sumAll = function (num1, num2) {
+  let start = Math.min(num1, num2);
+  let end = Math.max(num1, num2);
+  let result = 0;
+
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+
+  return result;
 };
+
+sumAll(-10, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
