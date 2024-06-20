646be85ca7bc1523a8a253378605dcdd9467b5a9
spcymshrm <joe.jordan1994@gmail.com>
2024-06-20 08:08:26 -0400

    Starrting work again

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,19 +2,7 @@
 // numeric numbers. Keep up the great work! BTW, use the ...rest argument and re-write
 // this assignment.
 
-const sumAll = function (num1, num2) {
-  let start = Math.min(num1, num2);
-  let end = Math.max(num1, num2);
-  let result = 0;
-
-  for (let i = start; i <= end; i++) {
-    result += i;
-  }
-
-  return result;
-};
-
-sumAll();
+const sumAll = function (...nums) {};
 
 // Do not edit below this line
 module.exports = sumAll;
